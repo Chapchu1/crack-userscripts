30c66226dd6e73be461d3e4e995c35e56d342fc0
maong <328710372+Chapchu1@users.noreply.github.com>
2026-09-26 04:26:30 +0900

    Add files via upload

--- crack-theme-background.meta.js
+++ crack-theme-background.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🖼️ CSP - Generated Image Background Blur (배경 이미지&테마)
 // @namespace    crack-scene-painter-background-borderless
-// @version      4.3.18
+// @version      4.3.19
 // @description  다크/라이트와 소설형/채팅형을 자동 구분해 배경·테마를 적용하고, PEARLWAVE와 해양 테마의 장식 및 채팅창 색상을 제공하며, 기존/추가 테마 접기와 시나모롤·치이카와·샤오랑·체리·키라라·세일러문 테마를 제공합니다.
 // @match        https://crack.wrtn.ai/*
 // @require      https://cdn.jsdelivr.net/npm/fflate@0.8.2/umd/index.js
